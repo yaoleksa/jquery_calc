a30c52c7a7fd23735373568c869454c462bcec0d
yaoleksa <mryaremchyk@gmail.com>
2020-03-16 14:59:04 +0000

    Comment

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const display = document.querySelector('input');
 let finished = false;
 let divisionByZero = false;
+let MS=0;
 
 document.querySelectorAll('.digits button').forEach(button=>button.addEventListener('click', digitPressed));
 
@@ -40,12 +41,25 @@ function clear() {
     display.value = '';
 }
 
+document.querySelector('.save').addEventListener('click', resultSave)
+
+function resultSave() {
+    MS=eval(display.value);
+    display.value='';
+}
+
+document.querySelector('.display').addEventListener('click', resultShow)
+
+function resultShow() {
+    display.value+=MS;
+}
+
 document.querySelector('.eq').addEventListener('click', calculate)
 
 function calculate() {
 
     if (display.value.includes("!")) {
-        const num = parseFloat(display.value.slice(0, display.value.indexOf('!')));
+        const num = eval(parseFloat(display.value.slice(0, display.value.indexOf('!'))));
         let factorial = 1;
         let descNum = num;
         while (descNum > 0) {
@@ -56,15 +70,15 @@ function calculate() {
     }
 
     if (display.value.includes("^")) {
-        const base = parseFloat(display.value.slice(0, display.value.indexOf('^')));
-        const exp = parseFloat(display.value.slice(display.value.indexOf('^') + 1));
+        const base = eval(parseFloat(display.value.slice(0, display.value.indexOf('^'))));
+        const exp = eval(parseFloat(display.value.slice(display.value.indexOf('^') + 1)));
         display.value = Math.pow(base, exp);
     }
     else if (eval(display.value) === Infinity) {
         display.value = "Division by zero! = infinity";
         divisionByZero = true;
     }
-    else if (String(eval(display.value) - parseInt(eval(display.value))).length > 7) {
+    else if (displat.value.slice(display.value.indexOf('.')+1).length > 7) {
         display.value = Number(eval(display.value)).toFixed(5);
     } else {
         display.value = eval(display.value);
