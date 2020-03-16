1bc8650897195bc674165a863d312ebc302515e1
yaoleksa <mryaremchyk@gmail.com>
2020-03-16 08:41:35 +0000

    Comment

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const display=document.querySelector('input');
-
+const result=document.querySelector('output .result.outres');
 document.querySelectorAll('.digits button')
     .forEach(button=>button.addEventListener('click', digitPressed));
 
@@ -12,4 +12,10 @@ document.querySelectorAll('.digits button')
 
     function digitPressed(ev){
        display.value+=ev.target.innerText;
+    }
+
+    document.querySelector('.eq').addEventListener('click', calculate)
+
+    function calculate(){
+        display.value = eval(display.value);
     }
